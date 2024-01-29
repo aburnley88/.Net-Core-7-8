710921e6e67044806b60ce00f7ddcbeba65eb151
Arnold Burnley <aburnleylvx@outlook.com>
2024-01-29 09:23:41 -0500

    used model binding

--- Controllers/Banking/BankingControllerExample/BankingControllerExample/Controllers/AccountController.cs
+++ Controllers/Banking/BankingControllerExample/BankingControllerExample/Controllers/AccountController.cs
@@ -20,14 +20,12 @@ namespace BankingControllerExample.Controllers
         [Route("account-statement")]
         public IActionResult GetDummyFile() => File("/cert.pdf", "application/pdf");
 
-        [Route("get-current-balance/{accountNumber:int:min(1)?}")]
-        public IActionResult GetCurrentBalance()
+        [Route("get-current-balance/{accountNumber:int:min(1)?}")]       
+        public IActionResult GetCurrentBalance([FromRoute] int accountNumber)
         {
-            var acctNum = Convert.ToInt32(Request.RouteValues["accountNumber"]);
-          
-            return acctNum == 0 ? NotFound("Account number should be provided") : 
-                (acctNum != 1001 ? BadRequest("Account number should be 1001") : Ok(5000));
-
+           // var acctNum = Convert.ToInt32(Request.RouteValues["accountNumber"]);
+            return accountNumber == 0 ? NotFound("Account number should be provided") : 
+                (accountNumber != 1001 ? BadRequest("Account number should be 1001") : Ok(5000));
         }
     }
 }
